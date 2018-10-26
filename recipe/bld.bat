1c58a7efcc1ea2903d02fdbfe2c9e5340c4f5afb
Jake Bolewski <jakebolewski@gmail.com>
2018-10-25 19:21:46 -0600

    re-render with windows build support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,15 +3,22 @@ setlocal EnableDelayedExpansion
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
-cmake -G "%CMAKE_GENERATOR%" ^
+cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DTILEDB_WERROR=OFF ^
+      -DTILEDB_TBB_SHARED=ON ^
       -DTILEDB_S3=ON ^
+      -DTILEDB_HDFS=OFF ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build . --target INSTALL --config Release
+nmake
 if errorlevel 1 exit 1
+
+pushd "tiledb"
+nmake install
+if errorlevel 1 exit 1
+popd
 
 popd
