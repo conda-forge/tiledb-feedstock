fe882b68cdd606fd26792a3e00b4a641d805c12b
Isaiah Norton <isaiah.norton@gmail.com>
2023-03-21 09:05:44 -0400

    Fix bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,13 +3,17 @@ setlocal EnableDelayedExpansion
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
+if "%gcs%"=="gcs_enabled" (
+    set TILEDB_GCS=ON
+)
+
 cmake -G "NMake Makefiles" ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DTILEDB_WERROR=OFF ^
       -DTILEDB_TBB_SHARED=ON ^
       -DTILEDB_AZURE=ON ^
-      -DTILEDB_GCS=ON ^
+      -DTILEDB_GCS=%TILEDB_GCS% ^
       -DTILEDB_S3=ON ^
       -DTILEDB_HDFS=OFF ^
       -DCOMPILER_SUPPORTS_AVX2=OFF ^
