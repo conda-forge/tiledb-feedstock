326343443a32326a65a4f02297a7aab809530b66
Isaiah Norton <isaiah.norton@gmail.com>
2023-07-15 10:44:59 -0400

    Use RECIPE_DIR for source

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -2,6 +2,6 @@
 
 # This test builds the contents of link_test_src, a simple test executable
 # linked against tiledb. Then runs the executable.
-cmake -B link_test_bld -S link_test_src
+cmake -B link_test_bld -S $RECIPE_DIR/link_test_src
 cmake --build link_test_bld
 cmake --build link_test_bld --target link_test -v
